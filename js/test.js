2552977a3e41274d8c56ee890bb42eb6a0dba881
Bowen Yu <yubowenok@gmail.com>
2014-10-28 03:12:42 -0400

    test code wrapper, easier to create default views

---
+++ js/test.js
@@ -0,0 +1,18 @@
+function createTestViews() {
+  // You can add your test views here to define the action for "Test Preset" button
+  // The views defined here will be launched when the system starts
+  createView("Chart", "chart").load();
+  createView("Network", "network").load();
+  createView("Genome Browser", "binding").load();
+
+  /*
+   * the following code create a network and load some genes (currently not working)
+   */
+  /*
+  var net1 = createView("Network1", "network");
+  net1.load({
+      network: "th17",
+      genesRegex: "^BATF$|^RORC$|^STAT3$|^FOSL2$|^MAF$|^IRF4$"
+    });
+  */
+}
