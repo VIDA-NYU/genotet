6f98c22c534c9b3c681f1228c810b7d06c72bc95
Shauro <shauroshanying@gmail.com>
2015-10-20 15:45:11 -0400

    Switched the panel from accordion to vertical tab

--- panel-manager/panel-manager.js
+++ panel-manager/panel-manager.js
@@ -1,29 +1,19 @@
-/*$(document).ready(function(){
-    $('#triangle-button').click(function(){
-        $('.panel-manager').animate({
-        	width: 'toggle'
-        });
-    });
-    $('#triangle-button').hover(function(){
-    	$(this).css('border-left-color', 'gray');
-    	}, function(){
-    	$(this).css('border-left-color', 'black');
-    });
-});*/
+$('#tabs').tabs().addClass('ui-tabs-vertical ui-helper-clearfix');
+$('#tabs li').removeClass('ui-corner-top').addClass('ui-corner-left');
 
 $('#triangle-button').button({
   icons: {primary: 'ui-icon-triangle-1-e'}
 });
 var icons = $('#triangle-button').button('option', 'icons');
-$('#triangle-button').button('option', 'icons', {primary: 'ui-icon-triangle-1-e'});
+$('#triangle-button').button('option', 'icons', { primary: 'ui-icon-triangle-1-e' });
 
 $(function(){
-    $('#triangle-button').on('click', function(){
-        $('.panel-manager').toggle('slide', {direction: 'right'}, 1000);
-        $('#triangle-button').animate({
-            'margin-right' : $('#triangle-button').css('margin-right') == '0px' ? '195px' : '0px'
-        }, 1000);
-    });
+  $('#triangle-button').on('click', function(){
+    $('.panel-manager').toggle('slide', { direction: 'right' }, 1000);
+    $('#triangle-button').animate({
+      'margin-right' : $('#triangle-button').css('margin-right') == '0px' ? '195px' : '0px'
+    }, 1000);
+  });
 });
 
 var count = 0;
@@ -31,20 +21,19 @@ var count = 0;
 $(document).ready(function(){
     $('#btn-add').click(function(){
     	count++;
-        $('#accordion').append("<div id='panel"+count+"'></div>");
-    	$('#panel'+count).append("<h3 id='panel-title"+count+"'><a href='#'>view"+count+"</a></h3>");
-        $('#panel'+count).append("<div id='panel-content"+count+"'>properties</div>");
+        $('#tabs ul').append("<li id='li"+count+"'><a href='#tabs-"+count+"'>view"+count+"</a></li>");
+    	$('#tabs').append("<div id='tabs-"+count+"'>");
+        $('#tabs-'+count).append("<h2>view"+count+"</h2>");
+        $('#tabs-'+count).append('<p>properties</p>');
+        $('#tabs').tabs('refresh');
+        $('#tabs').tabs({ active: -1 });
         $('#triangle-button').css('display', count > 0 ? 'inline' : 'none');
         $('.panel-manager').css('display', count > 0 ? 'inline' : 'none');
-
-        (function($) {
-            $(function() {
-                $('#accordion > div').accordion({header: 'h3', collapsible: true});
-            })
-        })(jQuery);
     });
     $('#btn-remove').click(function(){
-        $('#panel'+count).remove();
+        $('#li'+count).remove();
+        $('#tabs-'+count).remove();
+        $('#tabs').tabs("option", "active", $("#tabs").tabs('option', 'active') > 0 ? $("#tabs").tabs('option', 'active') - 1 : 0);
         count--;
         $('#triangle-button').css('display', count > 0 ? 'inline' : 'none');
         $('.panel-manager').css('display', count > 0 ? 'inline' : 'none');
