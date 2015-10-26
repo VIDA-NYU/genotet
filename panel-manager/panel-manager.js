96c25f720e666f489f00b500baa06608ea33b355
Shauro <shauroshanying@gmail.com>
2015-10-26 00:04:20 -0400

    Switched the vertical tab from jQuery UI to bootstrap

--- panel-manager/panel-manager.js
+++ panel-manager/panel-manager.js
@@ -1,41 +1,35 @@
-$('#tabs').tabs().addClass('ui-tabs-vertical ui-helper-clearfix');
-$('#tabs li').removeClass('ui-corner-top').addClass('ui-corner-left');
-
-$('#triangle-button').button({
-  icons: {primary: 'ui-icon-triangle-1-e'}
-});
-var icons = $('#triangle-button').button('option', 'icons');
-$('#triangle-button').button('option', 'icons', { primary: 'ui-icon-triangle-1-e' });
-
 $(function(){
-  $('#triangle-button').on('click', function(){
-    $('.panel-manager').toggle('slide', { direction: 'right' }, 1000);
-    $('#triangle-button').animate({
-      'margin-right' : $('#triangle-button').css('margin-right') == '0px' ? '195px' : '0px'
-    }, 1000);
+  $('#slidebar').on('click', function(){
+    $('#slider').toggle('slide', { direction: 'right' }, 300);
+    $('#slidebar').animate({
+      'right' : $('#slidebar').css('right') == '0px' ? '250px' : '0px'
+    }, 300);
+    setTimeout(function(){
+        $('#icon-button').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
+    }, 300);
   });
 });
 
-var count = 0;
+// var count = 0;
 
 $(document).ready(function(){
-    $('#btn-add').click(function(){
-    	count++;
-        $('#tabs ul').append("<li id='li"+count+"'><a href='#tabs-"+count+"'>view"+count+"</a></li>");
-    	$('#tabs').append("<div id='tabs-"+count+"'>");
-        $('#tabs-'+count).append("<h2>view"+count+"</h2>");
-        $('#tabs-'+count).append('<p>properties</p>');
-        $('#tabs').tabs('refresh');
-        $('#tabs').tabs({ active: -1 });
-        $('#triangle-button').css('display', count > 0 ? 'inline' : 'none');
-        $('.panel-manager').css('display', count > 0 ? 'inline' : 'none');
+    $('#btn-add').click(function(e){
+        // count++;
+        var count = $('.sideways').children().length;
+        e.preventDefault();
+        $('.sideways').append('<li id="li'+count+'"><a href="#view'+count+'" data-toggle="tab">view'+count+'</a></li>');
+        $('.tab-content').append('<div class="tab-pane" id="view'+count+'">property'+count+'</div>');
+        $('#li'+count+' a[href="#view'+count+'"]').tab('show');
+        $('.panel-manager').css('display', count > -1 ? 'inline' : 'none');
     });
     $('#btn-remove').click(function(){
+        var count = $('.sideways').children().length - 1;
+        var actived = false;
+        if ($('#li'+count).hasClass('active') == true){ actived = true; }
         $('#li'+count).remove();
-        $('#tabs-'+count).remove();
-        $('#tabs').tabs("option", "active", $("#tabs").tabs('option', 'active') > 0 ? $("#tabs").tabs('option', 'active') - 1 : 0);
+        $('#view'+count).remove();
         count--;
-        $('#triangle-button').css('display', count > 0 ? 'inline' : 'none');
-        $('.panel-manager').css('display', count > 0 ? 'inline' : 'none');
+        if (actived == true){ $('#li'+count+' a[href="#view'+count+'"]').tab('show'); }
+        $('.panel-manager').css('display', count > -1 ? 'inline' : 'none');
     });
 });
