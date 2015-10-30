e5cbb806f5eae878cc7cae51952505c91f9e3976
Shauro <shauroshanying@gmail.com>
2015-10-30 17:16:39 -0400

    Added callbacks of creatView, closeView and closeAllViews. Fixed toggleSlideBar function. Not tested yet.

--- panel-manager/panel-manager.js
+++ panel-manager/panel-manager.js
@@ -1,35 +1,44 @@
-$(function(){
-  $('#slidebar').on('click', function(){
-    $('#slider').toggle('slide', { direction: 'right' }, 300);
-    $('#slidebar').animate({
-      'right' : $('#slidebar').css('right') == '0px' ? '249px' : '0px'
-    }, 300);
-    setTimeout(function(){
-        $('#icon-button').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
-    }, 300);
-  });
-});
+function toggleSlideBar(){
+  $('#slider').toggle('slide', { direction: 'right' }, 300);
+  $('#slidebar').animate({
+    'right' : $('#slidebar').css('right') == '0px' ? '249px' : '0px'
+  }, 300);
+  setTimeout(function(){
+      $('#icon-button').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
+  }, 300);
+}
 
 // var count = 0;
 
 $(document).ready(function(){
-    $('#btn-add').click(function(e){
-        // count++;
-        var count = $('.sideways').children().length;
-        e.preventDefault();
-        $('.sideways').append('<li id="li'+count+'"><a href="#view'+count+'" data-toggle="tab">view'+count+'</a></li>');
-        $('.tab-content').append('<div class="tab-pane" id="view'+count+'">property'+count+'</div>');
-        $('#li'+count+' a[href="#view'+count+'"]').tab('show');
-        $('.panel-manager').css('display', count > -1 ? 'inline' : 'none');
-    });
-    $('#btn-remove').click(function(){
-        var count = $('.sideways').children().length - 1;
-        var actived = false;
-        if ($('#li'+count).hasClass('active') == true){ actived = true; }
-        $('#li'+count).remove();
-        $('#view'+count).remove();
-        count--;
-        if (actived == true){ $('#li'+count+' a[href="#view'+count+'"]').tab('show'); }
-        $('.panel-manager').css('display', count > -1 ? 'inline' : 'none');
-    });
+  $('#slidebar').on('click', toggleSlideBar);
+//  $('#btn-add').click(function(e){
+  $.when(ViewManager.createView(type, viewName, params)).done(function(e){
+//     count++;
+    var count = $('.sideways').children().length;
+    e.preventDefault();
+//    $('.sideways').append('<li id="li'+count+'"><a href="#view'+count+'" data-toggle="tab">view'+count+'</a></li>');
+    $('.sideways').append('<li id="li'+count+'"><a href="#view'+count+'" data-toggle="tab">'+viewName+'</a></li>');
+    $('.tab-content').append('<div class="tab-pane" id="view'+count+'">property'+count+'</div>');
+    $('#li'+count+' a[href="#view'+count+'"]').tab('show');
+    $('.panel-manager').css('display','inline');
+    if ($('#slidebar').css('right') == '0px'){ $('#slidebar').trigger('click'); }
+  });
+//  $('#btn-remove').click(function(e){
+  $.when(ViewManager.closeView(view)).done(function(e){
+    var count = $('.sideways').children().length - 1;
+    var actived = false;
+    if ($('#li'+count).hasClass('active') == true){ actived = true; }
+    $('#li'+count).remove();
+    $('#view'+count).remove();
+    count--;
+    if (actived == true){ $('#li'+count+' a[href="#view'+count+'"]').tab('show'); }
+    $('.panel-manager').css('display', count > -1 ? 'inline' : 'none');
+  });
+//  $('#btn-closeAll').click(function(e){
+  $.when(ViewManager.closeAllViews()).done(function(e){
+    $('.sideways').empty();
+    $('.tab-content').empty();
+    $('.panel-manager').css('display', 'none');
+  });
 });
