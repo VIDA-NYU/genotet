10f5924265cfdd27e75582b746d257d3e5962f11
Shauro <shauroshanying@gmail.com>
2015-11-02 19:57:50 -0500

    Fixed more JS file format. Added TODO(liana) in comments

--- js/panel-manager.js
+++ js/panel-manager.js
@@ -1,45 +1,52 @@
 var PanelManager = {
   TRANSITION_TIME: 300,
-  init: function(e){
-    $('.panel-manager').load('templates/panel-manager.html', function(e){
-      $('#slidebar').on('click', function(e){
+
+  init: function(e) {
+    $('.panel-manager').load('templates/panel-manager.html', function(e) {
+      $('#slidebar').on('click', function(e) {
         $('#slider').toggle('slide', { direction: 'right' }, PanelManager.TRANSITION_TIME);
         $('#slidebar').animate({
           'right' : $('#slidebar').css('right') == '0px' ? '249px' : '0px'
         }, PanelManager.TRANSITION_TIME);
-        setTimeout(function(){
+        setTimeout(function(e) {
           $('#icon-button').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
         }, PanelManager.TRANSITION_TIME);
       });
     });
   },
 
-  addPanel: function(view){
+  addPanel: function(view) {
     var count = $('.sideways').children().length;
     var viewID = view.name().replace(/\s/g, '');
-    $('.sideways').append('<li id="li'+viewID+'"><a href="#'+viewID+'" data-toggle="tab">'+view.name()+'</a></li>');
-    $('.tab-content').append('<div class="tab-pane" id="'+viewID+'">property'+count+'</div>');
-    $('#li'+viewID+' a[href="#'+viewID+'"]').tab('show');
+    $('.sideways').append('<li id="li' + viewID + '"><a href="#' + viewID + '" data-toggle="tab">' + view.name() + '</a></li>');
+    $('.tab-content').append('<div class="tab-pane" id="' + viewID + '">property' + count + '</div>');
+    $('#li' + viewID + ' a[href="#' + viewID + '"]').tab('show');
     $('.panel-manager').css('display','inline');
-    if ($('#slidebar').css('right') == '0px'){ $('#slidebar').trigger('click'); }
+    if ($('#slidebar').css('right') == '0px') {
+      $('#slidebar').trigger('click');
+    }
   },
 
-  removePanel: function(viewName){
+  removePanel: function(viewName) {
     var count = $('.sideways').children().length - 1;
     var actived = false;
     var viewID = viewName.replace(/\s/g, '');
-    if ($('#li'+viewID).hasClass('active') == true){ actived = true; }
-    $('#li'+viewID).remove();
-    $('#'+viewID).remove();
+    if ($('#li' + viewID).hasClass('active')) {
+      actived = true;
+    }
+    $('#li' + viewID).remove();
+    $('#' + viewID).remove();
     count--;
-    if (actived == true){ $('#li'+viewID+' a[href="#'+viewID+'"]').tab('show'); }
+    if (actived) {
+      $('#li' + viewID + ' a[href="#' + viewID + '"]').tab('show');
+    }
     $('.panel-manager').css('display', count > -1 ? 'inline' : 'none');
   }
 
+//  TODO(liana): Fixed closeAllPanels function.
 //  closeAllPanels: function(e){
 //    $('.sideways').empty();
 //    $('.tab-content').empty();
 //    $('.panel-manager').css('display', 'none');
 //  },
 };
-
