7b4e84c12d92c032beb71bb96b42a7290673a71e
Shauro <shauroshanying@gmail.com>
2015-11-02 19:44:28 -0500

    Used constants for the constant numbers. Removed unused codes in JS file

--- js/panel-manager.js
+++ js/panel-manager.js
@@ -1,58 +1,45 @@
-// var count = 0;
-
 var PanelManager = {
-//$(document).ready(function(){
-//  $('#btn-add').click(function(e){
+  TRANSITION_TIME: 300,
   init: function(e){
     $('.panel-manager').load('templates/panel-manager.html', function(e){
       $('#slidebar').on('click', function(e){
-        $('#slider').toggle('slide', { direction: 'right' }, 300);
+        $('#slider').toggle('slide', { direction: 'right' }, PanelManager.TRANSITION_TIME);
         $('#slidebar').animate({
           'right' : $('#slidebar').css('right') == '0px' ? '249px' : '0px'
-        }, 300);
+        }, PanelManager.TRANSITION_TIME);
         setTimeout(function(){
           $('#icon-button').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
-        }, 300);
+        }, PanelManager.TRANSITION_TIME);
       });
     });
   },
 
   addPanel: function(view){
-//     count++;
     var count = $('.sideways').children().length;
-//    e.preventDefault();
-//    $('.sideways').append('<li id="li'+count+'"><a href="#view'+count+'" data-toggle="tab">view'+count+'</a></li>');
-//    $('.sideways').append('<li id="li'+count+'"><a href="#view'+count+'" data-toggle="tab">'+viewName+'</a></li>');
     var viewID = view.name().replace(/\s/g, '');
     $('.sideways').append('<li id="li'+viewID+'"><a href="#'+viewID+'" data-toggle="tab">'+view.name()+'</a></li>');
     $('.tab-content').append('<div class="tab-pane" id="'+viewID+'">property'+count+'</div>');
     $('#li'+viewID+' a[href="#'+viewID+'"]').tab('show');
     $('.panel-manager').css('display','inline');
-//    $('.panel-manager').show();
     if ($('#slidebar').css('right') == '0px'){ $('#slidebar').trigger('click'); }
   },
-//  $('#btn-remove').click(function(e){
-//  $.when($.ajax(ViewManager.closeView())).done(function(e){
+
   removePanel: function(viewName){
     var count = $('.sideways').children().length - 1;
     var actived = false;
-//    if ($('#li'+count).hasClass('active') == true){ actived = true; }
     var viewID = viewName.replace(/\s/g, '');
     if ($('#li'+viewID).hasClass('active') == true){ actived = true; }
-//    $('#li'+count).remove();
     $('#li'+viewID).remove();
     $('#'+viewID).remove();
     count--;
     if (actived == true){ $('#li'+viewID+' a[href="#'+viewID+'"]').tab('show'); }
     $('.panel-manager').css('display', count > -1 ? 'inline' : 'none');
-//  });
   }
-//  $('#btn-closeAll').click(function(e){
+
 //  closeAllPanels: function(e){
-//  $.when(ViewManager.closeAllViews()).done(function(e){
 //    $('.sideways').empty();
 //    $('.tab-content').empty();
 //    $('.panel-manager').css('display', 'none');
-//  };
-//});
+//  },
 };
+
