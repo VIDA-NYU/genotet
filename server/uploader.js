52512fc4264a032bc2f77c5cf5bdfe515abe2913
djasmine <djmestery@gmail.com>
2015-11-16 01:41:47 -0500

    fix and test wiggle file convertion

--- server/uploader.js
+++ server/uploader.js
@@ -7,9 +7,12 @@
 var path;
 var fs = require('fs');
 var child = require('child_process');
+var rl = require('readline');
+var mkdirp = require('mkdirp');
 
 var utils = require('./utils.js');
 var segtree = require('./segtree.js');
+var uploader = require('./uploader.js');
 
 module.exports = {
 
@@ -27,23 +30,21 @@ module.exports = {
    */
   uploadFile: function(desc, file, prefix, bigWigToWigAddr) {
     var fileType = desc.type;
-    console.log(desc);
-    console.log(file);
     var source = fs.createReadStream(file.path);
     var dest = fs.createWriteStream(prefix + desc.name);
     source.pipe(dest);
-    source.on("end", function(){});
-    source.on("err", function(err){});
-    fs.unlink(file.path);
+    source.on('end', function() {
+      fs.unlink(file.path);
+      if (desc.type == 'binding') {
+        this.bigwigtoBcwig(prefix, desc.name, bigWigToWigAddr);
+      }
+    }.bind(this));
+    source.on('err', function(err){});
 
     // write down the network name and description
     var fd = fs.openSync(prefix + desc.name + ".txt", 'w');
     fs.writeSync(fd, desc.description);
     fs.closeSync(fd);
-
-    if (desc.type == 'binding') {
-      //this.bigwigtoBcwig(prefix, desc.name, bigWigToWigAddr);
-    }
 
     return {
       success: true
@@ -61,7 +62,7 @@ module.exports = {
     // convert *.bw into *.wig
     var wigFileName = bwFile + '.wig';
     console.log('start transfer');
-    //child.execSync(bigWigWoWigAddr + ' ' + prefix + bwFile + ' ' + prefix + wigFileName);
+    child.execSync(bigWigWoWigAddr + ' ' + prefix + bwFile + ' ' + prefix + wigFileName);
     console.log(bigWigWoWigAddr + ' ' + prefix + bwFile + ' ' + prefix + wigFileName);
 
     // convert *.wig into *.bcwig
@@ -74,69 +75,81 @@ module.exports = {
     seg['chrX'] = [];
     seg['chrY'] = [];
 
-    var buf = fs.readFileSync(prefix + wigFileName);
-    var wigLine = buf.toString().split('\n');
+    // solve each line
+    var lines = rl.createInterface({
+      input: fs.createReadStream(prefix + wigFileName),
+      terminal: false
+    });
     var lastxr = -1;
-    for (var i = 1; i < wigLine.length; i++) {
-      if (wigLine.contains('#')) {
-        continue;
-      }
-      var wigLinePart = wigLine.split(RegExp(/\s+/));
-      var chName = wigLinePart[0];
-      var xl = parseInt(wigLinePart[1]);
-      var xr = parseInt(wigLinePart[2]);
-      var val = parseFloat(wigLinePart[3]);
-      if (xl != lastxr) {
+    lines.on('line', function(line) {
+      if (line.indexOf('#') == -1) {
+        var linePart = line.split(RegExp(/\s+/));
+        var chName = linePart[0];
+        var xl = parseInt(linePart[1]);
+        var xr = parseInt(linePart[2]);
+        var val = parseFloat(linePart[3]);
+        //console.log(seg);
+        if (xl != lastxr && lastxr > -1) {
+          seg[chName].push({
+            x: lastxr,
+            val: 0
+          });
+        }
         seg[chName].push({
-          x: lastxr,
-          val: 0
+          x: xl,
+          val: val
         });
+        lastxr = xr;
       }
-      seg[chName].push({
-        x: xl,
-        val: val
-      });
-      lastxr = xr;
-    }
+    });
 
-    // write to *.bcwig file
-    var namecode = bwFile;
+    lines.on('close', function() {
+      // write to *.bcwig file
+      console.log('start log it');
+      // if the folder already exists, then delete it
+      var folder = prefix + bwFile + '_chr';
+      var stats = fs.lstatSync(folder);
+      if (stats.isDirectory()) {
+        fs.rmdirSync(folder);
+        console.log('Wiggle file ' + bwFile + ' is replaced.');
+      }
+      mkdirp(folder);
 
-    // if the folder already exists, then delete it
-    if (fs.exists(prefix + namecode)) {
-      fs.rmdirSync(prefix + namecode);
-      console.log('Wiggle file ' + namecode + ' is replaced.');
-    }
-    fs.mkdir(prefix + namecode);
-
-    for (var chr in seg) {
-      var bcwigFile = prefix + namecode + '/' + namecode + '_' + chr + '.bcwig';
-      for (var i = 0; i < seg[chr].length; i++) {
-        var bcwigBuf = new Buffer(8 * seg[chr].length);
-
-        bcwigBuf.writeInt32LE(seg[chr][i].x, i * 4);
-        bcwigBuf.writeFloatLE(seg[chr][i].val, i * 4 + 4);
+      for (var chr in seg) {
+        var bcwigFile = folder + '/' + bwFile + '_' + chr + '.bcwig';
         var fd = fs.openSync(bcwigFile, 'w');
-        fs.writeSync(fd, buf, 0, 4 * seg[chr].length, 0);
+        for (var i = 0; i < seg[chr].length; i++) {
+          var bcwigBuf = new Buffer(8 * seg[chr].length);
+          bcwigBuf.writeInt32LE(seg[chr][i].x, i * 4);
+          bcwigBuf.writeFloatLE(seg[chr][i].val, i * 4 + 4);
+        }
+        fs.writeSync(fd, bcwigBuf, 0, 8 * seg[chr].length, 0);
         fs.closeSync(fd);
       }
-    }
 
 
-    // build segment tree and save
-    for (var chr in seg) {
-      var segFile = prefix + namecode + '/' + namecode + '_' + chr + '.seg';
-      var nodes = [];
-      segtree.buildSegmentTree(nodes, seg[chr]);
-      var segBuf = new Buffer(4 + 4 * nodes.length);
-      segBuf.writeInt32LE(nodes.length, 0);
-      for (var i = 0, offset = 4; i < nodes.length; i++, offset += 2) {
-        segBuf.writeFloatLE(nodes[i], offset);
+      // build segment tree and save
+
+      for (var chr in seg) {
+        if (seg[chr].length == 0) {
+          console.log(chr + " has no binding data.");
+          continue;
+        }
+        var segFile = folder + '/' + bwFile + '_' + chr + '.seg';
+        var nodes = [];
+        segtree.buildSegmentTree(nodes, seg[chr]);
+        var segBuf = new Buffer(4 + 4 * nodes.length);
+          segBuf.writeInt32LE(nodes.length, 0);
+        for (var i = 0, offset = 4; i < nodes.length; i++, offset += 2) {
+          segBuf.writeFloatLE(nodes[i], offset);
+        }
+        var fd = fs.openSync(segFile, 'w');
+        fs.writeSync(fd, segBuf, 0, offset, 0);
+        fs.closeSync(fd);
       }
-      var fd = fs.openSync(segFile, 'w');
-      fs.writeSync(fd, segBuf, 0, offset, 0);
-      fs.closeSync(fd);
-    }
+
+    });
+
   }
 
 };
