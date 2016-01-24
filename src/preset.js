512c9a835f85db1b7e1e17fa16d94f82ff75e4d4
Shauro <shauroshanying@gmail.com>
2016-01-24 06:12:52 -0500

    Implemented preset layout.

--- src/preset.js
+++ src/preset.js
@@ -12,21 +12,69 @@ genotet.preset = {};
  * @param {string} preset
  */
 genotet.preset.loadPreset = function(preset) {
+  genotet.viewManager.closeAllViews();
+
   if (!preset) {
     preset = 'default';
   }
-
   switch (preset) {
     case 'default':
+      this.createExpression_();
+      this.createNetwork_();
+      this.createBinding_();
       break;
     case 'network':
+      this.createNetwork_();
+      this.createBinding_();
       break;
     case 'expression':
+      this.createExpression_();
+      this.createNetwork_();
       break;
     case 'binding':
+      this.createExpression_();
+      this.createNetwork_();
+      this.createBinding_();
       break;
     default:
       genotet.error('unknown preset:', preset);
       return;
   }
+};
+
+/**
+ * Create a preset expression view.
+ * @private
+ */
+genotet.preset.createExpression_ = function() {
+  genotet.viewManager.createView('expression', 'My Expression Matrix', {
+    fileName: 'expressionMatrix',
+    isGeneRegex: true,
+    isConditionRegex: true,
+    geneInput: 'sig.*',
+    conditionInput: 'si.*'
+  });
+};
+
+/**
+ * Create a preset network view.
+ * @private
+ */
+genotet.preset.createNetwork_ = function() {
+  genotet.viewManager.createView('network', 'My Network', {
+    fileName: 'meishei.tsv',
+    geneRegex: 'BATF|RORC|STAT3|IRF4|MAF'
+  });
+};
+
+/**
+ * Create a preset binding view.
+ * @private
+ */
+genotet.preset.createBinding_ = function() {
+  genotet.viewManager.createView('binding', 'My Genome Browser', {
+    fileName: 'b6.bw',
+    bedName: 'bed_data.bed',
+    chr: '1'
+  });
 };
