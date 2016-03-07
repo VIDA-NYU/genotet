31133dd349cb54e6706744960753b971a1142589
djasmine <djmestery@gmail.com>
2016-03-07 14:33:41 -0500

    add user log

--- server/log.js
+++ server/log.js
@@ -28,3 +28,31 @@ log.serverLog = function(var_args) {
   }
   console.log(content);
 };
+
+/**
+ * Prints user activity logs.
+ * @param {string} userPath Path to the user folder.
+ * @param {string} userName User name to be logged.
+ * @param {!Array<{
+ *   timestamp: number,
+ *   type: string,
+ *   action: string
+ * }>} logs Log information.
+ */
+log.userLog = function(userPath, userName, logs) {
+  if (!logs.length) {
+    return;
+  }
+  var folder = userPath + userName + '/';
+  var date = new Date();
+  var logFile = folder + 'log_' + dateFormat(date, 'yyyy-mm-dd_HH') + '.log';
+  var fd = fs.openSync(logFile, 'w+');
+  logs.forEach(function(log) {
+    var logString = '';
+    var date = new Date(log.timestamp);
+    logString += '[' + dateFormat(date, 'yyyy-mm-dd_HH:MM:ss') + ']\t';
+    logString += log.type + ' ' + log.action;
+    fs.writeSync(fd, logString + '\n');
+  });
+  fs.closeSync(fd);
+};
