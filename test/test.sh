d5e61953bda969752d7985833e9ea93ecefa7bbe
Bowen Yu <yubowenok@gmail.com>
2016-04-27 00:33:16 -0400

    use abort func in test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
 nohup node server/server.js --config=test/config &
+pid=$!
+res=0
+
+function abort {
+  kill $1
+  exit $2
+}
 gulp all
 res=$?
 if [[ $res -ne 0 ]]
 then
-  exit $res
+  abort $pid $res
 fi
 gulp dist
 res=$?
 if [[ $res -ne 0 ]]
 then
-  exit $res
+  abort $pid $res
 fi
 bash test/test-server.sh
 res=$?
 if [[ $res -ne 0 ]]
 then
-  exit $res
+  abort $pid $res
 fi
 gulp test
 res=$?
 if [[ $res -ne 0 ]]
 then
-  exit $res
+  abort $pid $res
 fi
-kill $!
+kill $pid
